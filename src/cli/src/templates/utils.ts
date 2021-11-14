9495a207aa5920f7745c88f39a33a9f55c527ce5
Joshua Wood <joshumcode@gmail.com>
2021-11-13 20:59:11 -0500

    changed formatting

--- src/cli/src/templates/utils.ts
+++ src/cli/src/templates/utils.ts
@@ -319,11 +319,8 @@ const pageModelArguments = (path: string[], options : Options) : string => {
 const exposes = (value : string) => (str : string) : boolean => {
   const regex = new RegExp('^module\\s+[^\\s]+\\s+exposing\\s+\\(((?:\\.\\)|[^)])+)\\)')
   const match = (str.match(regex) || [])[1]
-  if (match) {
-    return match.split(',').filter(a => a).map(a => a.trim()).includes(value)
-  } else {
-    return false
-  }
+  return match ? match.split(',').filter(a => a).map(a => a.trim()).includes(value)
+    : false
 }
 
 export const exposesModel = exposes('Model')
